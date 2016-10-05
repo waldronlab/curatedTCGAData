a9e356e583f58f810bbf068046c4982fb478c758
“Lucas <schifferl@users.noreply.github.com>
2016-10-05 17:50:50 -0400

    corrected metadata.csv file

---
+++ R/tcga_mae.R
@@ -0,0 +1,12 @@
+#' A fun function
+#' @export get_curatedTCGAData
+#read_csv("https://raw.githubusercontent.com/waldronlab/MultiAssayExperiment-TCGA/master/MAEOinfo.csv")
+get_curatedTCGAData <- function(cohort = "ACC", RNASeqGene = TRUE,
+                                RNASeq2GeneNorm = TRUE, miRNASeqGene = TRUE,
+                                CNASNP = TRUE, CNVSNP = TRUE, CNAseq = TRUE,
+                                Methylation = TRUE, RPPAArray = TRUE,
+                                Mutations = TRUE, gistica = TRUE,
+                                gistict = TRUE) {
+    #check the table
+
+}
