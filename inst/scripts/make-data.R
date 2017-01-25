0a9d7c9f92e02effc1495ca8d3fa2eba83865ad2
LiNk-NY <mramos09@gmail.com>
2017-01-25 13:16:54 -0500

    include make-data.R

---
+++ inst/scripts/make-data.R
@@ -0,0 +1,20 @@
+## The code use to run this TCGA pipeline and to create all the data objects
+## can be found in this GitHub repository:
+browseURL("https://github.com/waldronlab/MultiAssayExperiment-TCGA")
+
+## Note: The `MultiAssayExperiment-TCGA` repository has this GitHub package
+## dependency:
+browseURL("https://github.com/waldronlab/TCGAutils")
+
+## Please see the following script for installing the proper dependencies:
+browseURL("https://github.com/waldronlab/MultiAssayExperiment-TCGA/blob/master/R/installLibraries.R")
+
+## Some manual work may be required when downloaded the curated datasets
+## After all data is downloaded and in the proper folders, the user
+## only needs to run one command.
+
+## Move to the repository directory
+## system("cd MultiAssayExperiment-TCGA/exec")
+
+## Run the script that builds the datasets
+## system("./globalScript.sh")
