1d3966abfe9b15f4ca8be89807b2d0ba272516bd
LiNk-NY <mramos09@gmail.com>
2017-10-04 15:17:32 -0400

    update script for diseaseCodes

--- inst/scripts/getDiseaseCodes.R
+++ inst/scripts/getDiseaseCodes.R
@@ -1,10 +1,18 @@
-## Function for getting useable barcodes (no RTCGAToolbox dep)
-getDiseaseCodes <- function() {
-    data("diseaseCodes", package = "TCGAutils")
-    excludedCodes <- c("COADREAD", "GBMLGG", "KIPAN", "STES", "FPPP", "CNTL",
-                       "LCML", "MISC")
-    logicalSub <- !diseaseCodes[[1L]] %in% excludedCodes
-    diseases <- unname(unlist(diseaseCodes[logicalSub, 1L]))
-    names(diseases) <- diseases
-    diseases
-}
+## Extract cancer codes from TCGA project
+library(rvest)
+ccTable <- "https://gdc.cancer.gov/resources-tcga-users/tcga-code-tables/tcga-study-abbreviations"
+htcc <- read_html(ccTable)
+diseaseCodes <- html_table(htcc, fill = TRUE)[[2L]]
+names(diseaseCodes) <- make.names(colnames(diseaseCodes))
+
+excludedCodes <- c("COADREAD", "GBMLGG", "KIPAN", "STES", "FPPP", "CNTL",
+                   "LCML", "MISC")
+logicalSub <- !diseaseCodes[["Study.Abbreviation"]] %in% excludedCodes
+diseaseCodes[["Available"]] <- factor(logicalSub,  levels = c("TRUE", "FALSE"),
+    labels = c("Yes", "No"))
+
+diseaseCodes <- diseaseCodes[order(diseaseCodes[["Study.Abbreviation"]]), ]
+devtools::use_data(diseaseCodes, internal = TRUE, overwrite = TRUE)
+
+## For easy subsetting use:
+diseaseCodes[["Study.Abbreviation"]][diseaseCodes$Available == "Yes"]
