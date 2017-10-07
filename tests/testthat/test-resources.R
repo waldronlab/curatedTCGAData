ede29965fcb126af1e8efb51b1bee27f71b13900
LiNk-NY <mramos09@gmail.com>
2017-10-06 20:11:13 -0400

    reaname and add test

---
+++ tests/testthat/test-resources.R
@@ -0,0 +1,9 @@
+context("ensure resources are present")
+
+test_that("metadata numbers match ExperimentHub", {
+    assays_file <- system.file("extdata", "metadata.csv",
+        package = "curatedTCGAData", mustWork = TRUE)
+    metadataFile <- read.csv(assays_file)
+    EHub <- query(ExperimentHub(), "curatedTCGAData")
+    expect_equal(nrow(metadataFile), length(EHub))
+})
