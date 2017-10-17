96977c37d401bbee716dcb42ac79fb9b83d17b46
LiNk-NY <mramos09@gmail.com>
2017-10-16 20:10:24 -0400

    update test to adjust for extra resources

--- tests/testthat/test-resources.R
+++ tests/testthat/test-resources.R
@@ -3,7 +3,13 @@ context("ensure resources are present")
 test_that("metadata numbers match ExperimentHub", {
     assays_file <- system.file("extdata", "metadata.csv",
         package = "curatedTCGAData", mustWork = TRUE)
-    metadataFile <- read.csv(assays_file)
+    metadataFile <- read.csv(assays_file, stringsAsFactors = FALSE)
     EHub <- query(ExperimentHub(), "curatedTCGAData")
-    expect_equal(nrow(metadataFile), length(EHub))
+    ## SKCM patch
+    skc <- grepl("SKCM", metadataFile[["Title"]])
+    sameFiles <- grepl("colData|metadata|sampleMap",
+        metadataFile[skc, "Title"])
+    additional <- sum(skc) - sum(sameFiles)
+    ## END patch
+    expect_equal(nrow(metadataFile), length(EHub)-additional)
 })
