dd0db103bad5fb0bdd221eb1371743ee83449f3c
LiNk-NY <mramos09@gmail.com>
2017-10-23 20:38:48 -0400

    remove failing test, resources updated

--- tests/testthat/test-resources.R
+++ tests/testthat/test-resources.R
@@ -5,11 +5,5 @@ test_that("metadata numbers match ExperimentHub", {
         package = "curatedTCGAData", mustWork = TRUE)
     metadataFile <- read.csv(assays_file, stringsAsFactors = FALSE)
     EHub <- query(ExperimentHub(), "curatedTCGAData")
-    ## SKCM patch
-    skc <- grepl("SKCM", metadataFile[["Title"]])
-    sameFiles <- grepl("colData|metadata|sampleMap",
-        metadataFile[skc, "Title"])
-    additional <- sum(skc) - sum(sameFiles)
-    ## END patch
-    expect_equal(nrow(metadataFile), length(EHub)-additional)
+    expect_equal(nrow(metadataFile), length(EHub))
 })
