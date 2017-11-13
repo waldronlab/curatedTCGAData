a0970b3b2b75d67b04e6705d70ae49d1d05f2721
LiNk-NY <mramos09@gmail.com>
2017-11-13 12:33:22 -0500

    update format in R script

--- R/curatedTCGAData-package.R
+++ R/curatedTCGAData-package.R
@@ -14,42 +14,42 @@ NULL
 #'
 #' \preformatted{
 #'
-#'    Study Abbreviation                                                       Study Name
-#' --------------------------------------------------------------------------------------
-#' 1                 ACC                                         Adrenocortical Carcinoma
-#' 2                BLCA                                     Bladder Urothelial Carcinoma
-#' 3                BRCA                                        Breast Invasive Carcinoma
-#' 4                CESC Cervical Squamous Cell Carcinoma And Endocervical Adenocarcinoma
-#' 5                CHOL                                               Cholangiocarcinoma
-#' 6                CNTL                                                         Controls
-#' 7                COAD                                             Colon Adenocarcinoma
-#' 8                DLBC                  Lymphoid Neoplasm Diffuse Large B-cell Lymphoma
-#' 9                ESCA                                             Esophageal Carcinoma
-#' 10                GBM                                          Glioblastoma Multiforme
-#' 11               HNSC                            Head And Neck Squamous Cell Carcinoma
-#' 12               KICH                                               Kidney Chromophobe
-#' 13               KIRC                                Kidney Renal Clear Cell Carcinoma
-#' 14               KIRP                            Kidney Renal Papillary Cell Carcinoma
-#' 15               LAML                                           Acute Myeloid Leukemia
-#' 16                LGG                                         Brain Lower Grade Glioma
-#' 17               LIHC                                   Liver Hepatocellular Carcinoma
-#' 18               LUAD                                              Lung Adenocarcinoma
-#' 19               LUSC                                     Lung Squamous Cell Carcinoma
-#' 20               MESO                                                     Mesothelioma
-#' 21                 OV                                Ovarian Serous Cystadenocarcinoma
-#' 22               PAAD                                        Pancreatic Adenocarcinoma
-#' 23               PCPG                               Pheochromocytoma And Paraganglioma
-#' 24               PRAD                                          Prostate Adenocarcinoma
-#' 25               READ                                            Rectum Adenocarcinoma
-#' 26               SARC                                                          Sarcoma
-#' 27               SKCM                                          Skin Cutaneous Melanoma
-#' 28               STAD                                           Stomach Adenocarcinoma
-#' 29               TGCT                                      Testicular Germ Cell Tumors
-#' 30               THCA                                                Thyroid Carcinoma
-#' 31               THYM                                                          Thymoma
-#' 32               UCEC                             Uterine Corpus Endometrial Carcinoma
-#' 33                UCS                                           Uterine Carcinosarcoma
-#' 34                UVM                                                   Uveal Melanoma
+#'    Study Abbreviation                                            Study Name
+#' ---------------------------------------------------------------------------
+#' 1     ACC                                          Adrenocortical Carcinoma
+#' 2    BLCA                                      Bladder Urothelial Carcinoma
+#' 3    BRCA                                         Breast Invasive Carcinoma
+#' 4    CESC  Cervical Squamous Cell Carcinoma And Endocervical Adenocarcinoma
+#' 5    CHOL                                                Cholangiocarcinoma
+#' 6    CNTL                                                          Controls
+#' 7    COAD                                              Colon Adenocarcinoma
+#' 8    DLBC                   Lymphoid Neoplasm Diffuse Large B-cell Lymphoma
+#' 9    ESCA                                              Esophageal Carcinoma
+#' 10    GBM                                           Glioblastoma Multiforme
+#' 11   HNSC                             Head And Neck Squamous Cell Carcinoma
+#' 12   KICH                                                Kidney Chromophobe
+#' 13   KIRC                                 Kidney Renal Clear Cell Carcinoma
+#' 14   KIRP                             Kidney Renal Papillary Cell Carcinoma
+#' 15   LAML                                            Acute Myeloid Leukemia
+#' 16    LGG                                          Brain Lower Grade Glioma
+#' 17   LIHC                                    Liver Hepatocellular Carcinoma
+#' 18   LUAD                                               Lung Adenocarcinoma
+#' 19   LUSC                                      Lung Squamous Cell Carcinoma
+#' 20   MESO                                                      Mesothelioma
+#' 21     OV                                 Ovarian Serous Cystadenocarcinoma
+#' 22   PAAD                                         Pancreatic Adenocarcinoma
+#' 23   PCPG                                Pheochromocytoma And Paraganglioma
+#' 24   PRAD                                           Prostate Adenocarcinoma
+#' 25   READ                                             Rectum Adenocarcinoma
+#' 26   SARC                                                           Sarcoma
+#' 27   SKCM                                           Skin Cutaneous Melanoma
+#' 28   STAD                                            Stomach Adenocarcinoma
+#' 29   TGCT                                       Testicular Germ Cell Tumors
+#' 30   THCA                                                 Thyroid Carcinoma
+#' 31   THYM                                                           Thymoma
+#' 32   UCEC                              Uterine Corpus Endometrial Carcinoma
+#' 33    UCS                                            Uterine Carcinosarcoma
+#' 34    UVM                                                    Uveal Melanoma
 #' }
 #' @examples
 #' help(package = "curatedTCGAData")
