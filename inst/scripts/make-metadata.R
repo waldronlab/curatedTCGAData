33fc64f75fb3cfb8db77346cc4054b8cfa1a5524
LiNk-NY <mramos09@gmail.com>
2019-01-03 17:05:28 -0500

    update Maintainer fields

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,11 +1,12 @@
 make_metadata <- function(directory = "~/github/MultiAssayExperiment-TCGA/",
     dataDir = "data/bits", ext_pattern = allextpat,
-    resource_maintainer = read.dcf("DESCRIPTION", "Maintainer")[[1]],
+    resource_maintainer = utils::maintainer("curatedTCGAData"),
     resource_biocVersion = BiocManager::version()) {
     if (!dir.exists("inst/extdata"))
         dir.create("inst/extdata")
     if (file.exists("inst/extdata/metadata.csv"))
         file.remove("inst/extdata/metadata.csv")
+
     metadat <- getMetadata(directory = directory, dataDir = dataDir,
         ext_pattern = ext_pattern, resource_maintainer = resource_maintainer,
         resource_biocVersion = resource_biocVersion)
