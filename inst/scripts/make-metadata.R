9cc95490654c2401a358331887926605bd891afa
LiNk-NY <mramos09@gmail.com>
2019-01-08 13:53:52 -0500

    update metadata file and format make_metadata

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,14 +1,23 @@
-make_metadata <- function(directory = "~/github/MultiAssayExperiment-TCGA/",
-    dataDir = "data/bits", ext_pattern = allextpat,
+make_metadata <-
+function(
+    directory = "~/github/MultiAssayExperiment-TCGA/",
+    dataDir = "data/bits",
+    ext_pattern = allextpat,
     resource_maintainer = utils::maintainer("curatedTCGAData"),
-    resource_biocVersion = BiocManager::version()) {
-    if (!dir.exists("inst/extdata"))
-        dir.create("inst/extdata")
-    if (file.exists("inst/extdata/metadata.csv"))
-        file.remove("inst/extdata/metadata.csv")
+    resource_biocVersion = BiocManager::version())
+{
+    exdata <- "inst/extdata"
+    metafile <- file.path(exdata, "metadata.csv")
+
+    if (!dir.exists(exdata))
+        dir.create(exdata)
+
+    if (file.exists(metafile))
+        file.remove(metafile)
 
     metadat <- getMetadata(directory = directory, dataDir = dataDir,
         ext_pattern = ext_pattern, resource_maintainer = resource_maintainer,
         resource_biocVersion = resource_biocVersion)
+
     readr::write_csv(metadat, "inst/extdata/metadata.csv", col_names = TRUE)
 }
