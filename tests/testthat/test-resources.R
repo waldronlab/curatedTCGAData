09ad7a1717150d44e741df5fad1eb5bc1ded338b
LiNk-NY <mramos09@gmail.com>
2019-01-15 15:13:43 -0500

    bug fix: update metadata.csv file and tests

--- tests/testthat/test-resources.R
+++ tests/testthat/test-resources.R
@@ -5,10 +5,6 @@ test_that("metadata numbers match ExperimentHub", {
         package = "curatedTCGAData", mustWork = TRUE)
     metadataFile <- read.csv(assays_file, stringsAsFactors = FALSE)
     EHub <- query(ExperimentHub(), "curatedTCGAData")
-    ## SKCM patch
-    metaskc <- grepl("SKCM", metadataFile[["Title"]])
-    ehubskcm <- grepl("SKCM", EHub$title)
-    additional <- sum(ehubskcm) - sum(metaskc)
-    ## END patch
-    expect_equal(nrow(metadataFile), length(EHub)-additional)
+
+    expect_equal(nrow(metadataFile), length(EHub))
 })
