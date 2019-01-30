399d685ce11f090ba5a19f2b5a6df20c8bd03f34
LiNk-NY <mramos09@gmail.com>
2019-01-30 16:06:40 -0500

    update test to match EHub database

--- tests/testthat/test-resources.R
+++ tests/testthat/test-resources.R
@@ -3,12 +3,9 @@ context("ensure resources are present")
 test_that("metadata numbers match ExperimentHub", {
     assays_file <- system.file("extdata", "metadata.csv",
         package = "curatedTCGAData", mustWork = TRUE)
-    metadataFile <- read.csv(assays_file, stringsAsFactors = FALSE)
-    EHub <- query(ExperimentHub(), "curatedTCGAData")
-    ## SKCM patch
-    metaskc <- grepl("SKCM", metadataFile[["Title"]])
-    ehubskcm <- grepl("SKCM", EHub$title)
-    additional <- sum(ehubskcm) - sum(metaskc)
-    ## END patch
-    expect_equal(nrow(metadataFile), length(EHub)-additional)
+    meta <- read.csv(assays_file, stringsAsFactors = FALSE)
+    ehub <- query(ExperimentHub(), "curatedTCGAData")
+
+    ## expect that records in metadata and ExperimentHub
+    expect_equal(nrow(meta), length(ehub))
 })
