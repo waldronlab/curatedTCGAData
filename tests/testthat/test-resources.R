bef29674fedbd55c8b36d449e1caaf527d6f0cd7
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-04-26 19:41:16 -0400

    fix unit test to count actual resources and not "symlinks"

--- tests/testthat/test-resources.R
+++ tests/testthat/test-resources.R
@@ -4,6 +4,7 @@ test_that("metadata numbers match ExperimentHub", {
     assays_file <- system.file("extdata", "metadata.csv",
         package = "curatedTCGAData", mustWork = TRUE)
     metadataFile <- read.csv(assays_file, stringsAsFactors = FALSE)
+    actual_resources <- unique(metadataFile[["RDataPath"]])
     EHub <- query(ExperimentHub(), "curatedTCGAData")
-    expect_equal(nrow(metadataFile), length(EHub))
+    expect_equal(length(actual_resources), length(EHub))
 })
